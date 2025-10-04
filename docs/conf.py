0ff57ffdc9860ed35d9d39372d3dc7ab4dbe08c6
danutapfeiffer <danutapfeiffer@hotmail.com>
2025-10-04 16:10:05 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,9 +4,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-project = 'Safe PC'
+project = 'Look Webroot'
 copyright = '2025'
-author = 'Kristin Harmel'
+author = 'Leigh Behnke'
 
 release = '1.0'
 
@@ -21,3 +21,4 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
